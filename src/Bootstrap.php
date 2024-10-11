685b2468b2d41c66801815889ae08ba14727fe0b
Lila Karpowicz <lila@karpowicz.dev>
2024-10-11 15:15:59 +0000

    use dependency injector

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -20,8 +20,9 @@ if ($environment !== 'production') {
 
 $whoops->register();
 
-$request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
-$response = new \Http\HttpResponse;
+$injector = include('Dependencies.php');
+$request = $injector->make('Http\HttpRequest');
+$response = $injector->make('Http\HttpResponse');
 
 foreach ($response->getHeaders() as $header) {
     header($header, false);
@@ -39,6 +40,7 @@ $dispatcher = \FastRoute\simpleDispatcher($routeDefinitionCallback);
 $routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getPath());
 switch ($routeInfo[0]) {
     case \FastRoute\Dispatcher::NOT_FOUND:
+        echo 'HELLO 404';
         $response->setContent('404 - Page not found');
         $response->setStatusCode(404);
         break;
@@ -51,7 +53,7 @@ switch ($routeInfo[0]) {
         $method = $routeInfo[1][1];
         $vars = $routeInfo[2];
 
-        $class = new $className($response);
+        $class = $injector->make($className);
         $class->$method($vars);
         echo $response->getContent();
         break;
