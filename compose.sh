8161ed935c5d6b63ec89163011faf855c763ce14
Lila Karpowicz <lila@karpowicz.dev>
2024-10-19 10:56:36 +0000

    fix generation of mysql secrets for production

--- compose.sh
+++ compose.sh
@@ -4,24 +4,29 @@ set -e
 
 OS=$(uname)
 
-# Load production MySQL secrets into RAM.
+# Load production MySQL secrets from `pass` into RAM.
 if [[ "$OS" == "Linux" ]]; then
     DIR="/mnt/seguranca_web/secrets/"
 
     if [ ! -d "$DIR" ]; then
-        mkdir -p "$DIR"
+        sudo mkdir -p "$DIR"
     fi
 
-    if mount -t tmpfs -o size=500m --onlyonce tmpfs "$DIR"; then
-        echo "Mounted tmpfs at ${DIR}."
+    if mountpoint -q "$DIR"; then
+        echo "tmpfs is already mounted at ${DIR}."
     else
-        echo "Failed to mount tmpfs at ${DIR}." >&2
-        exit 1
+        if sudo mount -t tmpfs -o size=500m --onlyonce tmpfs "$DIR"; then
+            echo "Mounted tmpfs at ${DIR}."
+            chmod 0600 "$DIR"
+        else
+            echo "Failed to mount tmpfs at ${DIR}." >&2
+            exit 1
+        fi
     fi
 
     FILE="${DIR}mysql.prod.env"
-    if pass uac/seguranca_web/mysql.env > "$FILE"; then
-        chmod 0600 "$FILE"
+    if pass uac/seguranca_web/mysql.prod.env > "$FILE"; then
+        chmod 0400 "$FILE"
         echo "MySQL production secrets have been loaded into ${FILE}."
     else
         echo "Failed to load secrets into ${FILE}." >&2
@@ -31,3 +36,5 @@ else
     echo "This machine is running an unsupported operating system: ${OS}." >&2
     exit 1
 fi
+
+docker compose up
