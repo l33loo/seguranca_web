9689f47d692fc1bc3e1f2464a6eadcffa9bfdb7e
Lila Karpowicz <lila@karpowicz.dev>
2024-10-21 20:46:56 +0000

    specify all routes

--- app/src/Routes.php
+++ app/src/Routes.php
@@ -1,6 +1,23 @@
 <?php declare(strict_types = 1);
 
 return [
-    ['GET', '/', ['App\Controllers\Homepage', 'show']],
+    // Non-authenticated Users
+    ['GET', '/[activities]', ['App\Controllers\Homepage', 'show']],
+    // ['GET', '/activities/{id:\d+}', ['App\Controllers\Activity', 'show']],
+
+    // Authenticated Users - Clients and Vendors
+    // ['POST', '/login', ['App\Controllers\Homepage', 'show']],
+    // ['POST', '/logout', ['App\Controllers\Homepage', 'show']],
+    // ['GET', '/users/me/', ['App\Controllers\Homepage', 'show']],
+    // ['GET', '/reservations', ['App\Controllers\Homepage', 'show']],
+    // ['GET', '/reservations/{id:\d+}', ['App\Controllers\Homepage', 'show']],
+    // ['POST', '/reservations', ['App\Controllers\Homepage', 'show']],
     ['GET', '/{slug}', ['App\Controllers\Page', 'show']],
+
+    // Vendors only
+    // ['POST', '/activities', ['App\Controllers\Homepage', 'show']],
+    // ['PUT', '/activities/{id:\d+}', ['App\Controllers\Homepage', 'show']],
+    // ['PATCH', '/activities/{id:\d+}/archive', ['App\Controllers\Homepage', 'show']],
+    // ['GET', '/activities/{id:\d+}/reservations', ['App\Controllers\Homepage', 'show']],
+    // ['PATCH', '/reservations/{id:\d+}', ['App\Controllers\Homepage', 'show']],
 ];
