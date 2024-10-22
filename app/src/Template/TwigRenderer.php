5a983ba4a339348e630986d1afbe6095dc73306f
Lila Karpowicz <lila@karpowicz.dev>
2024-10-22 09:56:40 +0000

    replace Mustache wit Twig, and gitignore vendor/

---
+++ app/src/Template/TwigRenderer.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types = 1);
+
+namespace App\Template;
+
+use \Twig\Environment;
+
+class TwigRenderer implements Renderer
+{
+    private Environment $renderer;
+
+    public function __construct(Environment $renderer)
+    {
+        $this->renderer = $renderer;
+    }
+
+    public function render($template, $data = []): string
+    {
+        return $this->renderer->render("$template.html", $data);
+    }
+}
+
+
+
