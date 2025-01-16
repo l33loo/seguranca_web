97ce21f3b3a14c3d758cb7eb7513a04e38417e31
Lila Karpowicz <lila@karpowicz.dev>
2025-01-16 18:04:24 -0100

    fix permission issues

--- compose-production.sh
+++ compose-production.sh
@@ -17,17 +17,18 @@ if [[ "$OS" == "Linux" ]]; then
     else
         if sudo mount -t tmpfs -o size=500m --onlyonce tmpfs "$DIR"; then
             echo "Mounted tmpfs at ${DIR}."
-            sudo chmod 0600 "$DIR"
         else
             echo "Failed to mount tmpfs at ${DIR}." >&2
             exit 1
         fi
     fi
 
+    sudo chmod -R 0777 $DIR
+
     # Load MySQL secrets
     MYSQL_FILE="${DIR}mysql.prod.env"
-    if pass uac/seguranca_web/mysql.prod.env > "$MYSQL_FILE"; then
-        chmod 0400 "$MYSQL_FILE"
+    if pass uac/seguranca_web/mysql.prod.env > $MYSQL_FILE; then
+        # chmod 0400 "$MYSQL_FILE"
         echo "MySQL production secrets have been loaded into ${MYSQL_FILE}."
     else
         echo "Failed to load MySQL secrets into ${MYSQL_FILE}." >&2
@@ -35,9 +36,9 @@ if [[ "$OS" == "Linux" ]]; then
     fi
 
     # Load App secrets
-    APPL_FILE="${DIR}app.prod.env"
+    APP_FILE="${DIR}app.prod.env"
     if pass uac/seguranca_web/app.prod.env > "$APP_FILE"; then
-        chmod 0400 "$APP_FILE"
+        # chmod 0400 "$APP_FILE"
         echo "App production secrets have been loaded into ${APP_FILE}."
     else
         echo "Failed to load App secrets into ${APP_FILE}." >&2
